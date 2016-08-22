7982387962c127e4c2df4c9ed0986fbe514dad0c
Adrien Gautier <adr.gautier@gmail.com>
2016-08-22 13:59:51 +0200

    Demo page update
    Major update for demo/documentation page
    Some fixes

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -3,7 +3,15 @@ var browserSync = require('browser-sync').create();
 
 var stylus = require('gulp-stylus');
 var plumber = require('gulp-plumber');
+var pleeease = require('gulp-pleeease');
 
+// set minifier to false to keep Sass sourcemaps support
+var PleeeaseOptions = {
+    import: {"path": "assets/stylus"},
+    optimizers: {
+        minifier: false
+    }
+};
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['stylus'], function() {
@@ -21,6 +29,7 @@ gulp.task('stylus', function() {
     return gulp.src("assets/stylus/main.styl")
         .pipe(plumber())
         .pipe(stylus())
+        .pipe(pleeease(PleeeaseOptions))
         .pipe(gulp.dest("assets/css"))
         .pipe(browserSync.stream());
 });
